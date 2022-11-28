3386ef7d3a1b86614a941af368de8214f19e7378
Luan Costa <luan_costa@atlantico.com.br>
2022-11-28 16:47:19 -0300

    configurando EF

---
+++ Infra/DBConfiguration/EFCore/ApplicationContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using reality_subscribe_api.Model;
+
+namespace Infra.DBConfiguration.EFCore
+{
+    public class ApplicationContext : DbContext
+    {
+        /* Creating DatabaseContext without Dependency Injection */
+        public ApplicationContext() { }
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            if (!dbContextOptionsBuilder.IsConfigured)
+            {
+                dbContextOptionsBuilder.UseSqlServer(DatabaseConnection.ConnectionConfiguration
+                                                    .GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        /* Creating DatabaseContext configured by Dependency Injection */
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> User { get; set; }
+        public DbSet<Inscricao> TaskToDo { get; set; }
+    }
+}
