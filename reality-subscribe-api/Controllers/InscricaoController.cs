357f591b2fd60127576b1a0acc7972685935d592
Luan Costa <luan_costa@atlantico.com.br>
2022-12-02 18:52:43 -0300

    criação de controller de inscriçao

---
+++ reality-subscribe-api/Controllers/InscricaoController.cs
@@ -0,0 +1,26 @@
+﻿using Application.UseCases.Inscricao.Create;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace reality_subscribe_api.Controllers
+{
+    public class InscricaoController : RealityControllerBase
+    {
+        public readonly IMediator _mediator;
+        public InscricaoController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateInscricaoCommand inscricao)
+        {
+            var validationResult = await _mediator.Send(inscricao);
+            if (!validationResult.IsValid)
+            {
+                return ValidationError(validationResult.Errors);
+            }
+            return Ok(validationResult);
+        }
+    }
+}
---
+++ reality-subscribe-api/Controllers/RealityControllerBase.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using Models.ViewModel;
+using System.Net;
+
+namespace reality_subscribe_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RealityControllerBase : ControllerBase
+    {
+        protected ErrorDetails GenerateValidationProblemDetails(List<ValidationFailure> errors, string method = "")
+        {
+            var errorMessage = errors.FirstOrDefault()?.ErrorMessage;
+            return GenerateValidationProblemDetails(errorMessage, method);
+        }
+
+        private ErrorDetails GenerateValidationProblemDetails(string errorMessage, string method)
+        {
+            return new ErrorDetails()
+            {
+                StatusCode = (int)HttpStatusCode.BadRequest,
+                Message = errorMessage
+            };
+        }
+        protected ActionResult ValidationError(List<ValidationFailure> errors)
+        {
+            var errorDetails = GenerateValidationProblemDetails(errors);
+            if(errorDetails == null)
+            {
+                throw new ValidationException(nameof(errorDetails));
+            }
+            return new BadRequestObjectResult(errorDetails);
+        }
+    }
+}
